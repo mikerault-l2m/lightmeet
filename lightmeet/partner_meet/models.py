9d65e807a13ab2679ec782302aa0d47791066853
Mike <boudengenmickael@yahoo.fr>
2024-04-10 14:29:55 +0000

    Transfert front-end

--- lightmeet/partner_meet/models.py
+++ lightmeet/partner_meet/models.py
@@ -5,8 +5,7 @@ from django.db import models
 
 class PartnerMeet(models.Model):
     nom = models.CharField(max_length=100)
-    logo = models.ImageField(upload_to='MeetSites/', null=True, blank=True)  # Champ pour le logo du site: 200*200
-    url = models.URLField(blank=True) # URL de redirection du site web a associé à la variable a 'affiliation'
+    logo = models.ImageField(upload_to='MeetSites/', null=True, blank=True)
     GENRE_FIND_CHOICES = (
         ("Homme", "Je cherche un homme"),
         ("Femme", "Je cherche une femme"),
@@ -17,6 +16,8 @@ class PartnerMeet(models.Model):
     RELATION_CHOICES = (
         ('durables', 'Durables'),
         ('Relation d\'un soir', 'Relation d\' un soir'),
+        ('gays','Gays'),
+        ('lesbiennes','Lesbiennes'),
         ('toutes', 'Toutes')
     )
     relation = models.CharField(max_length=50, choices=RELATION_CHOICES)
@@ -30,24 +31,17 @@ class PartnerMeet(models.Model):
     )
     age = models.CharField(max_length=50, choices=AGE_CHOICES)
 
-    prix_avg = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    innovation = models.TextField(null=True, blank=True) # Mise en valeur de la marque.
+    url = models.URLField(blank=True) # URL de redirection du site web a associé à la variable a 'affiliation' - 1 - Site
+    description = models.TextField(null=True, blank=True) # Courte description du site de rencontres - 2 - Points forts en quelques mots
+    prix_avg = models.DecimalField(max_digits=10, decimal_places=2, default=0) # 2 Points forts : Prix
+    tranche_age = models.CharField(max_length=50) # 3 Membres - # BIAIS D'ECHANTILLONAGE : Pourcentage de la tranche d'âge majoritaire sur la plateforme.
+    nombre_visiteurs_par_mois = models.CharField(max_length=100)# - 3 Membres - # Cette variable comptabilise tous les 3 mois le nombre de visiteur du site web.
+    google_trustpilot_avg = models.FloatField(null=True, blank=True) # 4 - Note Utilisateur
+    affiliation = models.BooleanField(default=False)# 5 - Sélection cocher # Ce partenaire est affilié à la société Listen2Meet par un contrat d'affiliation.
 
     ranking = models.PositiveIntegerField(null=True, blank=True) # Ce numéro positionne la variable selon l'algo de pertinence.
 
-    affiliation = models.BooleanField(default=False) # Ce partenaire est affilié à la société Listen2Meet par un contrat d'affiliation.
-    categorie = models.CharField(max_length=100) # Ceci corresponds à la catégorie du site web.
-
-
-    nombre_visiteurs_par_mois = models.PositiveIntegerField() # Cette variable comptabilise tous les 3 mois le nombre de visiteur du site web.
-
     pourcentage_femmes = models.FloatField(null=True, blank=True) # PROBLEME : Cet indicateur indique aux célibataires recherchant des femmes le % de femmes présente sur le site.
-    tranche_age = models.CharField(max_length=50) # BIAIS D'ECHANTILLONAGE : Pourcentage de la tranche d'âge majoritaire sur la plateforme.
-    google_trustpilot_avg = models.FloatField(null=True, blank=True) # BIAIS D'ECHANTILLONAGE :
-
-    description = models.TextField(null=True, blank=True) # Courte description du site de rencontres
-
-    #storytelling_comment = models.TextField(null=True, blank=True) # Le remmplissage de cet indicateur reste complexe et chronophage.
 
     date_creation = models.DateTimeField(auto_now_add=True)
 
